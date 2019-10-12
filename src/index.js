cf5ef56d432ad544861d4ec3ed7a6b7181fa1d5e
KseniaYatskevich <kseniayatskevich@gmail.com>
2019-10-13 00:50:59 +0300

    fix: change solution

--- src/index.js
+++ src/index.js
@@ -3,16 +3,26 @@ module.exports = function check(str, bracketsConfig) {
   
   const brackets = str.split('');  
   
-  for (let j = 0; j < bracketsConfig.length; j++) {
+  for (let i = 0; i < brackets.length; i++) {
     
-    for ( let i = 0; i < brackets.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
       
       if (brackets[i] === bracketsConfig[j][0] && brackets[i+1] === bracketsConfig[j][1]){
-        brackets.splice(i, 2);       
-      }                 
+        brackets.splice(i, 2);
+        i=i-2;               
+        break;
+      }  
     } 
-
+    
   } 
+  //  console.log(brackets);  
+
+        
+   
+  
+  console.log(brackets);   
+
+// console.log(brackets); 
  
 
   if (brackets.length == 0) { 
