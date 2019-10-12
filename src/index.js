d243077dd5119806b9d0976d4141783cef5ec29d
KseniaYatskevich <kseniayatskevich@gmail.com>
2019-10-12 23:10:11 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  
+  
+  const brackets = str.split('');  
+  
+  for (let j = 0; j < bracketsConfig.length; j++) {
+    
+    for ( let i = 0; i < brackets.length; i++) {
+      
+      if (brackets[i] === bracketsConfig[j][0] && brackets[i+1] === bracketsConfig[j][1]){
+        brackets.splice(i, 2);       
+      }                 
+    } 
+
+  } 
+ 
+
+  if (brackets.length == 0) { 
+    return true;
+  } return false;
+  
+    
+  
+}  
+  
+  
+
+
